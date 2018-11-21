ba6029ae0844564650ac848852557d85aacca77a
Rafael La Buonora <nada@fana.com>
2018-11-21 19:10:49 +0000

    Validate presence of fields

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,29 @@
 describe User do
 
-  before(:each) { @user = User.new(email: 'user@example.com') }
+  before(:each) { @user = User.new(email: 'user@example.com',
+                                   name: 'Example User',
+                                   institucion: 'STPC') }
 
   subject { @user }
 
   it { should respond_to(:email) }
+  it { should respond_to(:name) }
+  it { should respond_to(:institucion) }
 
   it "#email returns a string" do
     expect(@user.email).to match 'user@example.com'
   end
 
+  it "requires name" do
+    @user.name = ""
+    @user.valid?
+    expect(@user.errors[:name].any?).to eq(true)
+  end
+  
+  
+  it "requires institucion" do
+    @user.institucion = ""
+    @user.valid?
+    expect(@user.errors[:institucion].any?).to eq(true)
+  end
 end
--- spec/support/helpers/session_helpers.rb
+++ spec/support/helpers/session_helpers.rb
@@ -1,8 +1,10 @@
 module Features
   module SessionHelpers
-    def sign_up_with(email, password, confirmation)
+    def sign_up_with(email, nombre, institucion, password, confirmation)
       visit new_user_registration_path
       fill_in 'Email', with: email
+      fill_in 'Nombre', with: nombre
+      fill_in 'Institucion', with: institucion
       fill_in 'Password', with: password
       fill_in 'Password confirmation', :with => confirmation
       click_button 'Sign up'
