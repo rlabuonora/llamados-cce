22054d122a3704591373590e5c2439b5ecd9ff44
Rafael La Buonora <nada@fana.com>
2018-12-07 20:54:13 +0000

    Spec for evaluacion

---
+++ spec/features/users/eval_new_spec.rb
@@ -0,0 +1,32 @@
+include Warden::Test::Helpers
+
+feature 'Evaluate propuesta', :devise do
+    
+    
+ scenario 'user evalua propuestas' do
+    user = FactoryBot.create(:user)
+    login_as(user, :scope => :user)
+    
+    c = Call.create(nombre: "Vidrieras", centro: "Rivera")
+
+
+    Proposal.create(proveedor: "Kolping", call: c)
+    Proposal.create(proveedor: "Thinkify", call: c)
+    
+    visit(call_path(c))
+    click_link "Ver Propuestas"
+    click_link "Evaluar"
+    
+    fill_in "Experiencia con Población Objetivo", with: 10
+    fill_in "Formacion", with: 10
+    fill_in "Experiencia Laboral", with: 10
+    
+    fill_in "Entrevista", with: 10
+    
+    click_button "Guardar Evaluacion"
+    
+    expect(current_path).to eq(call_path(call))
+    
+ end
+    
+end
