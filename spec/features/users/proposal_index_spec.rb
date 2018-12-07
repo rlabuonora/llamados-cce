204d953d51ad6a0ec0360d6cc216231238bce122
Rafael La Buonora <nada@fana.com>
2018-12-07 18:49:05 +0000

    Propuestas!

---
+++ spec/features/users/proposal_index_spec.rb
@@ -0,0 +1,29 @@
+include Warden::Test::Helpers
+
+
+feature 'Show proposals for a call', :devise do
+    
+  scenario 'list' do
+    call1 = Call.new(nombre: "Vidrieras", centro: "Tacuarembó")
+    prop1 = Proposal.create(call: call1, proveedor: "Kolping")
+    prop2 = Proposal.create(call: call1, proveedor: "Gepián")
+    
+    call2 = Call.new(nombre: "Gestión", centro: "Rivera")
+    prop3 = Proposal.create(call: call2, proveedor: "Thinkify")
+    prop4 = Proposal.create(call: call2, proveedor: "CPE")
+    
+    user = FactoryBot.create(:user)
+    login_as(user, :scope => :user)
+    
+    #visit call_proposals_url(call1)
+    
+    visit call_url(call1)
+    
+    click_link "Ver Propuestas"
+    expect(page).to have_text("Kolping")
+    expect(page).to have_text("Gepián")
+    
+    expect(page).not_to have_text("CPE")
+    
+  end
+end
---
+++ spec/features/users/proposal_new_spec.rb
@@ -0,0 +1,26 @@
+include Warden::Test::Helpers
+
+
+feature 'New proposal for a call', :devise do
+    
+  scenario 'list' do
+    call = Call.create(nombre: "Vidrieras", centro: "Tacuarembó")
+    
+    
+    user = FactoryBot.create(:user)
+    login_as(user, :scope => :user)
+    
+    visit call_url(call)
+    
+    click_link "Agregar Propuesta"
+    
+    fill_in "Proveedor", with: "Kolping"
+    
+    click_button "Salvar Propuesta"
+    
+    expect(current_path).to eq(call_proposals_path(call))
+    
+
+    
+  end
+end
