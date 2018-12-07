65b08224ac43d32f5003b60019229ebc1f20f46b
Rafael La Buonora <nada@fana.com>
2018-12-07 17:00:47 +0000

    New call

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -3,4 +3,21 @@ class CallsController < ApplicationController
     def index
       @calls = Call.all
     end
+    
+    def show
+      @call = Call.find(params[:id])
+    end
+    
+    def new
+      @call = Call.new
+    end
+    
+    def create
+      call_params = params.require(:call).permit(:nombre, :centro)
+      @call = Call.new(call_params)
+      @call.save
+      redirect_to @call
+    end
+    
+    
 end
