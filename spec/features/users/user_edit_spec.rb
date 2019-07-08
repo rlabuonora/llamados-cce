bc0862f26b26621cde15bc78cce2af1a75b23a70
rafa <rlabuonora@yahoo.com>
2019-07-08 17:45:02 -0300

    Cambiar formulario

--- spec/features/users/user_edit_spec.rb
+++ spec/features/users/user_edit_spec.rb
@@ -25,7 +25,7 @@ feature 'User edit', :devise do
     select 'DINAPYME', from: 'Institucion'
     fill_in 'Email', :with => 'newemail@example.com'
     fill_in 'Password Actual', :with => user.password
-    click_button 'Update'
+    click_button 'Actualizar'
     txts = [I18n.t( 'devise.registrations.updated'), I18n.t( 'devise.registrations.update_needs_confirmation')]
     expect(page).to have_content(/.*#{txts[0]}.*|.*#{txts[1]}.*/)
   end
